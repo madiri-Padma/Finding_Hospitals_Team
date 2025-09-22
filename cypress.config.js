5b03efde6745ecdce7e6728ab1ed34b28b3b379f
Padma <padma.madiri@cognizant.com>
2025-09-22 15:50:04 +0530

    mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,28 @@
-const { defineConfig } = require("cypress");
+// const { defineConfig } = require("cypress");
 
+// module.exports = defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+// });
+
+const { defineConfig } = require('cypress');
+ 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+ reporter: 'cypress-mochawesome-reporter',
+ reporterOptions: {
+   reportDir: 'cypress/reports',
+   charts: true,
+   reportPageTitle: 'Test Report',
+   embeddedScreenshots: true,
+   inlineAssets: true
+ },
+ video: true, // Enables video recording
+ e2e: {
+   setupNodeEvents(on, config) {
+     require('cypress-mochawesome-reporter/plugin')(on);
+   }
+ }
 });
---
+++ readme.md
@@ -0,0 +1,12 @@
+Get the hospital names 
+1. Hospital near which is open 24*7
+2. Which has parking facility
+3. rating should be more than 3.5
+(Suggested Site:practo.com however  you are free to choose any other legitimate  site)
+Detailed Description: Hackath Ideas
+ 
+1.1.In Bangalore, we need to create a list of hospital locations based on the areas displayed on the Practo website. 
+2.Bangalore city, identify ten Hospitals that is Open 24/7, rating more than 3.5; Display the hospital names                                                                     
+3.Go to Corporate Wellness, fill invalid details, schedule & capture the warning message from the alert
+4.Verify that the Practo homepage contains a valid LinkedIn link to the official company profile.
+
